c3926459619cc4c612e8c16148c2e65bb997b1e1
Dr.Mohmmed <mohmmed9770@gmail.com>
2023-08-21 16:05:59 +0200

    second commit

--- public/no-api/taxi-queue.no-api.js
+++ public/no-api/taxi-queue.no-api.js
@@ -3,49 +3,39 @@ document.addEventListener('alpine:init', () => {
 	Alpine.data('TaxiQueue', () => {
 
 		return {
+			passenger_count: 0,
+			taxi_count: 0,
+			departTaxi: 0,
+			joinQueue() {
+				this.passenger_count ++;
+			},
+			leaveQueue() {
+				if (this.passenger_count > 0) {
+					this.passenger_count--;
+				}
+			},
+			joinTaxiQueue() {
+				this.taxi_count++;
+				
+			},
+			queueLength() {
+				return passenger_count;
+			},
+			taxiQueueLength() {
+				return taxi_count;
+			},
+			taxiDepart() {
+				if (this.passenger_count >= 12 && this.taxi_count > 0) {
+					this.passenger_count -= 12;
+					this.taxi_count--;
+					this.departTaxi += 1;
+				}
+			},
 			version: 'no-api-1.0'
 		}
-
 	});
 
-
-	function TaxiQueue() {
-
-
-		function joinQueue() {
-	
-		}
-	
-		function leaveQueue() {
-	
-		}
-	
-		function joinTaxiQueue() {
-	
-		}
-	
-		function queueLength() {
-	
-		}
-	
-		function taxiQueueLength() {
-	
-		}
-	
-		function taxiDepart(){
-	
-		}
-	
-		return {
-			joinQueue,
-			leaveQueue,
-			joinTaxiQueue,
-			queueLength,
-			taxiQueueLength,
-			taxiDepart
-		}
-	}
-
 });
+
 
 
